53b7dd4636d98e5e7ccc0b7a30ea2cc5cde239ca
dukja <100657522+dukja@users.noreply.github.com>
2023-09-11 00:05:39 +0900

    first

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,46 +1,231 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/ui.css';
+import React, { useRef,useState ,useEffect, Children} from "react";
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Fade from '@mui/material/Fade';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
+const theme = createTheme({
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+  },
+});
 
-function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+interface ContentBoxProps {
+  preMsg: string;
+  conditionData: any;
+  contentData: any;
+  children?: React.ReactNode;
+}
 
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
-  }, []);
 
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
+
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+  interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
+  }
+function ScrollTop(props:Props) {
+
+  
+  const { window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
   };
 
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Box>
+    </Fade>
+  );
+}
+function App(props: Props) {
+  const [loading, setLoading] = useState(false);
+  const [nodeStyle, setNodeStyle] = useState(null);
+  const [nodeName, setNodeName] = useState(null);
+  const [seletedNode, setNodeSelected] = useState(null);
+
+  const [value, setValue] = React.useState(0);
+  const nodeStyleContentRef = useRef(null);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
 
-  React.useEffect(() => {
-    // This is how we read messages sent from the plugin controller
+  useEffect(() => {
     window.onmessage = (event) => {
-      const { type, message } = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
-        console.log(`Figma Says: ${message}`);
+      console.log("Received message in App.tsx", event.data);
+      const { data: eventData } = event;
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_style') {
+        setNodeStyle(eventData.pluginMessage.nodeStyle);
+        setLoading(false);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_name') {
+        setNodeName(eventData.pluginMessage.nodeName);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_selected') {
+        setNodeSelected(eventData.pluginMessage.seletedNode);
       }
     };
   }, []);
 
+  const handleLoading = () => {
+    setLoading(true);
+    window.parent.postMessage({ pluginMessage: { type: 'request_info' } }, '*');
+  }
+
+
+  const handleCopy = () => {
+    const nodeStyleContentRef = useRef<HTMLPreElement | null>(null);
+    const content = nodeStyleContentRef.current?.textContent;
+
+    if (content) {
+        const el = document.createElement('textarea');
+        el.value = content;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        alert("Copied to clipboard!");  // 복사 완료 
+    } else {
+        alert("Failed to copy!");  // 복사 실패
+    }
+}
+function openExternalLink(url:any) {
+  window.parent.postMessage({ pluginMessage: { type: 'open-external-link', url: url } }, '*');
+}
+const ContentBox: React.FC<ContentBoxProps> = ({preMsg, conditionData, contentData, children}) => {
+  return(
+    <Grid container rowSpacing={1}>
+      <Grid item xs={12} >   
+        <Typography color="text.secondary" variant="caption"><pre ref={nodeStyleContentRef} id="nodeStyleContent">    
+        {conditionData ?  JSON.stringify(contentData, null, 2): preMsg}</pre></Typography> 
+        {children}
+      </Grid>
+    </Grid> 
+)}
+
+function handleGetInfo () {
+  window.parent.postMessage({ pluginMessage: { type: 'request_selected' } }, '*');
+}
   return (
-    <div>
-      <img src={logo} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
-      <button onClick={onCancel}>Cancel</button>
-    </div>
+    <ThemeProvider theme={theme}>
+        <main>
+          <Container maxWidth="md">
+            <Box sx={{position: 'fixed',top:"20px", left:"16px",right:"16px" ,backgroundColor:"rgba(255, 255, 255, 0.8)"}}  >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Stack direction={"row"} alignItems="center" justifyContent="space-between" spacing={2} sx={{backgroundColor:"rgba(0, 0, 0, 0.05)",padding:"12px", borderRadius:"12px"}}>
+                    <Typography color="info" variant="caption">
+                      https://jsonviewer.stack.hu/
+                    </Typography>
+                    <Stack direction={"row"} spacing={1}>
+                    {
+                       nodeStyle? 
+                       <Button onClick={handleCopy} variant="contained">Copy</Button>:
+                        (loading? 
+                          <LoadingButton loading loadingPosition="start" startIcon={<SaveIcon />} variant="contained">Loading</LoadingButton>:
+                          <Button onClick={handleLoading} variant="outlined">Start</Button>
+                        ) 
+                    }
+                    </Stack>
+                  </Stack>
+                </Grid>                 
+                <Grid item xs={12}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                      <Tab label="style" {...a11yProps(0)} />
+                      <Tab label="name" {...a11yProps(1)} />
+                      <Tab label="selected" {...a11yProps(2)} />
+                    </Tabs>
+                </Grid>
+              </Grid>        
+            </Box>
+            <Box id="back-to-top-anchor" />
+            <Box sx={{marginTop:"114px"}}>
+              <CustomTabPanel value={value} index={0}>
+                <ContentBox conditionData={nodeStyle} contentData={nodeStyle} children={undefined} preMsg={`[START]를 클릭하고 기다려주세요`}/>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={1}>
+                <ContentBox conditionData={nodeStyle} contentData={nodeName} children={undefined} preMsg={`[START]를 클릭하고 기다려주세요`}/>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={2}>
+                <ContentBox conditionData={seletedNode} contentData={seletedNode} preMsg={`에셋을 선택하고 [GET INFO]을 클릭해 주세요`}>
+                  <Button onClick={handleGetInfo} variant="contained">GET INFO</Button>
+                </ContentBox>
+              </CustomTabPanel>
+            </Box>
+          </Container>
+          <ScrollTop {...props}/>        
+        </main>
+    </ThemeProvider>
   );
 }
 
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,209 @@
-figma.showUI(__html__);
+// 제외 페이지명
+const excludedPageNames = [
+  "A1",
+  "A2",
+  "🚫",
+  "▼",
+  "C1",
+  "Container",
+  "Cover",
+  "Scroll",
+  "제작중",
+  "Heading"
+];
+// 포함 노드 타입
+const includedNodeTypes = ["COMPONENT_SET", "COMPONENT","INSTANCE"];
+// 제외 노드명
+const excludedNodeNames = ["example", "document", "sample", "dev", "_","-dev","Guide","guide"];
+// 필터된 페이지
+let removePage:any = null;
+function getremovePage() {
+  if (removePage === null) {
+    removePage = figma.root.children.filter((page) => !isExcludedPage(page));
+  }
+  return removePage;
+}
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = [];
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+// 제외 페이지
+function isExcludedPage(node: any) {
+  return (
+    node.type === "PAGE" &&
+    excludedPageNames.some((name) => node.name.includes(name))
+  );
+}
+// 포함 노드
+function isIncludedNode(node: any) {
+  return (
+    includedNodeTypes.some((type) => node.type.includes(type)) &&
+    !excludedNodeNames.some((name) => node.name.includes(name))
+  );
+}
+// 스타일 노드
+function getStyledNode(node: any): any {
+  let styles = {
+    fill: null,
+    stroke: null,
+    effect: null
+  };
 
-    // This is how figma responds back to the ui
-    figma.ui.postMessage({
-      type: 'create-rectangles',
-      message: `Created ${msg.count} Rectangles`,
-    });
+  if (node.fillStyleId) {
+    styles.fill = node;
+  }
+  if (node.strokeStyleId) {
+    styles.stroke = node;
+  }
+  if (node.effectStyleId) {
+    styles.effect = node;
   }
 
-  figma.closePlugin();
+  if ("children" in node) {
+    for (let child of node.children) {
+      let styledChild = getStyledNode(child);
+      if (styledChild.fill && !styles.fill) {
+        styles.fill = styledChild.fill;
+      }
+      if (styledChild.stroke && !styles.stroke) {
+        styles.stroke = styledChild.stroke;
+      }
+      if (styledChild.effect && !styles.effect) {
+        styles.effect = styledChild.effect;
+      }
+    }
+  }
+
+  return styles;
+}
+
+
+// 스타일 이름
+function getStyleName(styleId:any) {
+  const style = figma.getStyleById(styleId);
+  return style ? style.name : null;
+}
+
+const addStyleNameToNodeStyles = (styleId: any, targetArray: any) => {
+  // 노드 스타일 이름
+  const styleName = getStyleName(styleId);
+  if (styleName && !targetArray.includes(styleName)) {
+    return [...targetArray, styleName];
+  }
+  return targetArray;
 };
+type NodeStyle = {
+  name: string;
+  fill: string[];
+  stroke: string[];
+  effect: string[];
+  defaultVariant: string | null;
+};
+
+type PageNodeInfo = {
+  pageName: string;
+  nodes: NodeStyle[];
+};
+
+// 노드 정보
+let nodesinfos: any[] = [];  // 배열로 초기화
+
+
+// 각 페이지의 노드 정보
+function getNodeInfo(){
+  if (nodesinfos.length > 0) return;
+  const removedPage = getremovePage();
+  removedPage.forEach((page :any) => {
+    //필터된 노드
+    const removedNode = page.children.filter((node :any) => isIncludedNode(node));
+    const nodesinfo: PageNodeInfo = {
+      pageName: page.name,
+      nodes: [],
+    };    
+    removedNode.forEach((node:any) => {
+      const nodeInfo = { name: node.name, fill: [], stroke: [], effect: [], defaultVariant: node.defaultVariant?node.defaultVariant.name:null};
+      if ("children" in node) {
+
+        node.children.forEach((childNode:any) => {  
+          //스타일 노드
+          const styleNodes = getStyledNode(childNode);
+          //노드 스타일 정보
+          if (styleNodes.fill) {
+            nodeInfo.fill=addStyleNameToNodeStyles(styleNodes.fill.fillStyleId, nodeInfo.fill);
+          }
+          if (styleNodes.stroke) {
+            nodeInfo.stroke=addStyleNameToNodeStyles(styleNodes.stroke.strokeStyleId, nodeInfo.stroke);
+          }
+          if (styleNodes.effect) {
+            nodeInfo.effect=addStyleNameToNodeStyles(styleNodes.effect.effectStyleId, nodeInfo.effect);
+          }
+        });
+
+        nodesinfo.nodes = [...nodesinfo.nodes,nodeInfo]
+      }
+    });
+    nodesinfos = [...nodesinfos , nodesinfo]
+  });
+}
+
+// 선택 노드 정보
+let selectednNodeinfos: any[] = [];  // 배열로 초기화
+function getSelectedNodeInfo (selectednNode:any) {
+  const  selectednNodeinfo = {name: selectednNode.name, fill: [], stroke: [], effect: [], defaultVariant: selectednNode.defaultVariant?selectednNode.defaultVariant.name:null}
+  if ("children" in selectednNode) {
+    selectednNode.children.forEach((childNode:any) => {  
+    //스타일 노드
+    const styleNodes = getStyledNode(childNode);
+    //노드 스타일 정보
+    if (styleNodes.fill) {
+      selectednNodeinfo.fill=addStyleNameToNodeStyles(styleNodes.fill.fillStyleId, selectednNodeinfo.fill);
+    }
+    if (styleNodes.stroke) {
+      selectednNodeinfo.stroke=addStyleNameToNodeStyles(styleNodes.stroke.strokeStyleId, selectednNodeinfo.stroke);
+    }
+    if (styleNodes.effect) {
+      selectednNodeinfo.effect=addStyleNameToNodeStyles(styleNodes.effect.effectStyleId, selectednNodeinfo.effect);
+    }
+  });}
+  selectednNodeinfos = [selectednNodeinfo]
+}
+
+
+
+
+figma.showUI(__html__, { width: 900, height: 600, title: "CDS Asset Filter" });
+
+
+figma.ui.onmessage = (message) => {
+
+  if (message.type === 'open-external-link') {
+    figma.notify(`브라우저에서 붙여넣기`);
+  }
+
+  if (message.type === 'request_info') {
+    if(nodesinfos.length === 0){
+      getNodeInfo()
+    }
+  }  
+    figma.ui.postMessage({
+      type: 'get_style',
+      nodeStyle: nodesinfos
+    });
+    const nodeNames = nodesinfos.map(pc => ({
+      pageName: pc.pageName,
+      nodeNames: pc.nodes.map((node: { name: any; }) => node.name),
+    }));      
+    figma.ui.postMessage({
+      type: 'get_name',
+      nodeName: nodeNames
+    });
+    if (message.type === 'request_selected') {
+      if(figma.currentPage.selection[0] !== null){
+        getSelectedNodeInfo(figma.currentPage.selection[0])
+      }
+    }  
+    figma.ui.postMessage({
+      type: 'get_selected',
+      seletedNode: selectednNodeinfos
+    });
+
+}
