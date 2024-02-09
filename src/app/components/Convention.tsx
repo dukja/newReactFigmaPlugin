2daf905e992b65510aec99fb92d9f61da1801699
dukja <100657522+dukja@users.noreply.github.com>
2024-02-10 02:15:15 +0900

    first

--- src/app/components/Convention.tsx
+++ src/app/components/Convention.tsx
@@ -5,28 +5,28 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, fat , protein) {
+  return { name, fat , protein };
 }
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Stack', '-', 'div'),
+  createData('컴포넌트명-{id 구분자}', 'Button-{id 메뉴}', '사양서'),
+  createData('Grid {xs 숫자}', 'Grid {xs 12} {md 12} {lg 12} {xl 12}','각각 총합 12'),
+  createData('Grid {container}',  '-', '-'),
+  createData('th {colspan 숫자}',  'th {colspan 2} {rowspan 2}', '-'),
+  createData('td {colspan 숫자}',  'td {colspan 2} {rowspan 2}', '-'),
+  createData('Image-Stack',  '-', '-'),
 ];
 
-export default function SpacingTable (){
+export default function Convention (){
     return(
         <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ minWidth: 400 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Nodes</TableCell>
+              <TableCell align="right">Sample</TableCell>
+              <TableCell align="right">Description</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -38,9 +38,7 @@ export default function SpacingTable (){
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
                 <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
                 <TableCell align="right">{row.protein}</TableCell>
               </TableRow>
             ))}
