5a3f99e4723c5020ae610d417410cddcf223e63a
dukja <100657522+dukja@users.noreply.github.com>
2024-02-10 23:54:45 +0900

    convention edited

--- src/app/components/Convention.tsx
+++ src/app/components/Convention.tsx
@@ -11,8 +11,8 @@ function createData(name, fat , protein) {
 const rows = [
   createData('Stack', '-', 'div element'),
   createData('컴포넌트명-{id 구분자}', 'Button-{id 메뉴}', '사양서'),
-  createData('Grid {xs 숫자}', 'Grid {xs 12} {md 12} {lg 12} {xl 12}','반응형 레이아웃, 스타일 없음'),
-  createData('Grid {container}',  '-', '반응형 레이아웃, 스타일 없음'),
+  createData('Grid {xs 숫자}', 'Grid {xs 12} {md 12} {lg 12} {xl 12}','반응형 레이아웃, 스타일 불가'),
+  createData('Grid {container}',  '-', '반응형 레이아웃, 스타일 불가'),
   createData('th {colspan 숫자}',  'th {colspan 2} {rowspan 2}', '테이블 헤더 셀'),
   createData('td {colspan 숫자}',  'td {colspan 2} {rowspan 2}', '테이블 바디 셀'),
   createData('Image-Stack',  '-', 'Image 부모 노드'),
