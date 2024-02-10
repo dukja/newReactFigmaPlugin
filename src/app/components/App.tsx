4082e5a5401a483b598ce6830f0cbb3ac46c277b
dukja <100657522+dukja@users.noreply.github.com>
2024-02-11 00:47:58 +0900

    origin

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,65 +1,130 @@
-import React,{useState} from "react";
-import {Tabs,Tab,Button,Stack,FormControl,FormLabel,FormControlLabel,Radio,Container,RadioGroup,FormHelperText} from '@mui/material';
+import React, { useRef,useState ,useEffect} from "react";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
 import {CustomTabPanel, a11yProps} from "./CustomTabPanel"
 import {ScrollTop, Props} from "./ScrollTop"
-import SpacingTable from "./Convention";
 function App(props: Props) {
-  const [nodeType, setNodeType] = useState('');
+  const [nodeInfos, setNodeInfos] = useState(null);
+  const [nodeNames, setNodeName] = useState(null);
+  const [nodeProperty, setProperty] = useState(null);
+  const [seletedNode, setNodeSelected] = useState(null);
   const [value, setValue] = React.useState(0);
+  const styleContentRef = useRef<HTMLPreElement | null>(null);
+  const nameContentRef = useRef<HTMLPreElement | null>(null);
+  const selectedContentRef = useRef<HTMLPreElement | null>(null);
 
-  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+
+  useEffect(() => {
+    window.onmessage = (event) => {
+      const { data: eventData } = event;
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_infos') {
+        setNodeInfos(eventData.pluginMessage.nodesInfos);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_names') {
+        setNodeName(eventData.pluginMessage.nodeNames);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_Property') {
+        setProperty(eventData.pluginMessage.property);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_selected') {
+        setNodeSelected(eventData.pluginMessage.seletedNode);
+      }
+      if (eventData.pluginMessage && eventData.pluginMessage.type === 'get_reset') {
+        setNodeSelected(eventData.pluginMessage.seletedNode);
+      }
+    };
+  }, []);
+
+  const handleChange = (_event: React.SyntheticEvent,newValue: number) => {
     setValue(newValue);
   };
 
-  const handleNodeTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNodeType(event.target.value); 
-  };
-
-  function handleCreate() {
-    window.parent.postMessage({ pluginMessage: { type: 'request_create', nodeType: nodeType } }, '*');
+  const handleInfos = () => {
+    window.parent.postMessage({ pluginMessage: { type: 'request_infos' } }, '*');
   }
 
+  const handleReset = () => {
+    window.parent.postMessage({ pluginMessage: { type: 'request_reset' } }, '*');
+  }
+
+  function handleGetInfo () {
+    window.parent.postMessage({ pluginMessage: { type: 'request_selected' } }, '*');
+  }
+
+  const handleCopy = async () => {
+    let content;
+
+    if (value === 0) {
+        content = styleContentRef.current?.textContent;
+    } else if (value === 1) {
+        content = nameContentRef.current?.textContent;
+    } else if (value === 2) {
+        content = selectedContentRef.current?.textContent;
+    }
+    if (content) {
+      const el = document.createElement('textarea');
+      el.value = content;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      alert("Copied to clipboard!");  // 복사 완료 
+    } else {
+        alert("Failed to copy!");  // 복사 실패
+    }
+  };
+
+  const CtrlNodes = ()=>{
+    return(
+      <Stack direction={"row"} spacing={1} sx={{backgroundColor:"rgba(0, 0, 0, 0.05)",padding:"6px", borderRadius:"12px"}}>
+        <Button onClick={handleInfos} disabled={nodeInfos!==null || nodeInfos}>{nodeInfos? `Loaded`:`Loading`}</Button>
+        <Button onClick={handleCopy} disabled={!nodeInfos}>Copy</Button>
+      </Stack>
+    )
+  }
+
+
   return (
-    <>
-      <Container maxWidth="md">
-        <Stack sx={{position: 'fixed',top:"0px" ,backgroundColor:"rgba(255, 255, 255, 0.8)"}}  >
-          <Tabs value={value} onChange={handleChange} aria-label="">
-            <Tab label="create node" {...a11yProps(0)} />
-            <Tab label="convention" {...a11yProps(1)} />
-          </Tabs>
-        </Stack>
-        <Stack id="back-to-top-anchor" />
-        <Stack sx={{marginTop:"48px"}}>
-          <CustomTabPanel value={value} index={0}>
-          <Stack direction={"row"} spacing={1} sx={{backgroundColor:"rgba(0, 0, 0, 0.05)",padding:"6px", borderRadius:"12px"}}>
-            <Button onClick={handleCreate} disabled={!nodeType}>Create</Button>
-          </Stack>
-          <FormControl sx={{marginTop:"16px"}}>
-            <FormLabel id="demo-radio-buttons-group-label">에셋을 선택하고 Node Type을 선택한 후 생성해 주세요</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
-              onChange={handleNodeTypeChange} 
-            >
-              <FormControlLabel value="Stack" control={<Radio />} label="Stack" />
-              <FormControlLabel value="컴포넌트명-{id 구분자}" control={<Radio />} label="컴포넌트명-{id 구분자}" />
-              <FormControlLabel value="Grid {xs 숫자}" control={<Radio />} label="Grid {xs 숫자}" />
-              <FormControlLabel value="Grid {container}" control={<Radio />} label="Grid {container}" />
-              <FormControlLabel value="th {colspan 숫자} {rowspan 숫자}" control={<Radio />} label="th {colspan 숫자} {rowspan 숫자}" />
-              <FormControlLabel value="td {colspan 숫자} {rowspan 숫자}" control={<Radio />} label="td {colspan 숫자} {rowspan 숫자}" />
-              <FormControlLabel value="Image-Stack" control={<Radio />} label="Image-Stack" />
-            </RadioGroup>
-            <FormHelperText>한글로 된 부분을 반영해 주세요</FormHelperText>
-          </FormControl>
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={1}>
-            <SpacingTable/>
-          </CustomTabPanel>
-          </Stack>
-      </Container>
-      <ScrollTop {...props}/>     
-    </>
+        <>
+          <Container maxWidth="md">
+            <Stack sx={{position: 'fixed',top:"0px" ,backgroundColor:"rgba(255, 255, 255, 0.8)"}}  >
+              <Tabs value={value} onChange={handleChange} aria-label="">
+                <Tab label="all nodes" {...a11yProps(0)} />
+                <Tab label="names" {...a11yProps(1)} />
+                <Tab label="property" {...a11yProps(1)} />
+                <Tab label="selected node" {...a11yProps(2)} />
+              </Tabs>
+            </Stack>
+            <Stack id="back-to-top-anchor" />
+            <Stack sx={{marginTop:"48px"}}>
+              <CustomTabPanel value={value} index={0}>
+                <CtrlNodes/>
+                <Typography ref={styleContentRef} id="nodeStyleContent"  color="text.secondary"  sx={{whiteSpace:"pre"}} variant="caption">{nodeInfos &&  JSON.stringify(nodeInfos, null, 2)}</Typography>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={1}>
+                <CtrlNodes/>
+                <Typography ref={nameContentRef} id="nodeStyleContent" color="text.secondary" sx={{whiteSpace:"pre"}} variant="caption">{nodeInfos && JSON.stringify(nodeNames, null, 2)}</Typography>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={2}>
+                <CtrlNodes/>
+                <Typography ref={selectedContentRef} id="nodeStyleContent" color="text.secondary" sx={{whiteSpace:"pre"}} variant="caption">{nodeInfos &&  JSON.stringify(nodeProperty, null, 2)}</Typography>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={3}>
+                <Stack direction={"row"} spacing={1} sx={{backgroundColor:"rgba(0, 0, 0, 0.05)",padding:"6px", borderRadius:"12px"}}>
+                    <Button onClick={handleGetInfo} {...a11yProps(2)} disabled={seletedNode !== null}>selected Style</Button>
+                    <Button onClick={handleReset} disabled={!seletedNode}>Reset</Button>
+                    <Button onClick={handleCopy} disabled={!seletedNode}>Copy</Button>
+                </Stack>
+                <Typography ref={selectedContentRef} id="nodeStyleContent" color="text.secondary" sx={{whiteSpace:"pre"}} variant="caption">{seletedNode &&  JSON.stringify(seletedNode, null, 2)}</Typography>
+              </CustomTabPanel>
+              </Stack>
+          </Container>
+          <ScrollTop {...props}/>     
+        </>
   );
 }
 export default App;
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,47 +1,263 @@
 
-function setNode(name) {
-  const frame = figma.createFrame();
-  return Object.assign(frame,{  
-    name : name, 
-    layoutMode : "HORIZONTAL", 
-    primaryAxisSizingMode : "AUTO",
-    counterAxisSizingMode : "AUTO", 
-    fills : [{
-      type: 'SOLID', 
-      color: { r: 1, g: 1, b: 1 }, 
-      opacity: 0
-    }]
-  }); 
+// 제외 페이지명
+const excludedPageNames = ['A1', 'A2', '▼', 'C1', 'Container', 'Cover', 'Scroll', '(작업중)','(검토중)','Heading', '📌'];
+// 포함 노드타입
+const includedNodeTypes = ['COMPONENT_SET', 'COMPONENT','SECTION'];
+// 제외 노드명
+const excludedNodeNames = ['example', 'document', 'sample', 'dev', '_', '-dev', 'Guide', 'guide'];
+
+function getPages() {
+  return figma.root.children.filter((node) => node.type === 'PAGE' && !excludedPageNames.some((name) => node.name?.includes(name)));
 }
 
-function getStack(type) {
-  let frame = setNode(type);
-  const selectedNode = figma.currentPage.selection[0]; 
-  if (selectedNode) {
-    frame.appendChild(selectedNode);
+function getNodes(node: any) {
+  return (
+    includedNodeTypes.some((type) => node.type.includes(type)) && !excludedNodeNames.some((name) => node.name?.includes(name))
+  );
+}
+
+function getStyledNode(node: any): any {
+  let styles = {
+    fill: undefined,
+    fills: undefined,
+    stroke: undefined,
+    strokes: undefined,
+    effect: undefined,
+    text: undefined,
+    textStyle: undefined,
+    radius: undefined,
+    padding: undefined,
+  };
+
+  if (node.fillStyleId) {
+    styles.fill = node;
   }
+  if (node?.fills?.[0]?.boundVariables?.['color']?.id) {
+    styles.fills = node;
+  }
+  if (node.strokeStyleId) {
+    styles.stroke = node;
+  }
+  if (node?.strokes?.[0]?.boundVariables?.['color']?.id ) {
+    styles.strokes = node;
+  }
+  if (node.effectStyleId) {
+    styles.effect = node;
+  }
+  if ('textStyleId' in node) { 
+    styles.text = node;
+  }  
+  if ('textStyleId' in node) { 
+    styles.textStyle = node;
+  }  
+  if (node.topLeftRadius || node.topRightRadius || node.bottomLeftRadius || node.bottomRightRadius) {
+    styles.radius = node;
+  }
+  if (node.paddingBottom || node.paddingLeft || node.paddingRight || node.paddingTop) {
+    styles.padding = node;
+  }
+  if (node?.children?.length > 0) {
+    node.children.forEach(child => {
+      let childStyles = getStyledNode(child);
+      Object.keys(styles).forEach(key => {
+        if (!styles[key] && childStyles[key]) {
+          styles[key] = childStyles[key];
+        }
+      });
+    });
+  }
+  return styles;
 }
-// 이미지 일때 내부 노드
-// 스택 상태 노드
-// 그리드 샘플
-// 테이블 샘플
-// 이미지 샘플
-// 스페이싱
-// asset
-// 텍스트 입력
-// 버튼 상태 변경
-// 샘플 노드 처리, slot 노드 처리
-// 재사용 컴포넌트 처리
-// 리뷰 컴포넌트
-// 컨벤션
 
+function getArray(array, item) {
+  if (Array.isArray(item)) {
+    let itemExists = array.some(subArray => {
+      return subArray.length === item.length && subArray.every((element, index) => {
+        return element === item[index];
+      });
+    });
+    if (!itemExists) {
+      array=[...array,item]
+    }
+  }
+  if (!Array.isArray(item) && !array.includes(item)) {
+    array=[...array,item]
+  }
+  return array;
+}
 
-figma.showUI(__html__, { width: 600, height: 520, title: 'Create Node' });
+function getStyleName(styleId: string): string | undefined {
+  return typeof styleId === 'string' ? figma.getStyleById(styleId)?.name : undefined;
+}
+
+function setNodeInfo(nodes,info){
+    if(Array.isArray(nodes.children)){
+      nodes.children.forEach((childNode: any) => {
+        const styleNodes = getStyledNode(childNode);
+        if (nodes.name) {
+          info.name = nodes.parent.type === 'COMPONENT_SET'? getArray(info.name,nodes.parent.name): getArray(info.name,nodes.name);
+        }
+        if (styleNodes.fill) {
+          info.fill = getArray(info.fill,getStyleName(styleNodes.fill.fillStyleId));
+        }
+        if (styleNodes.fills) {
+          info.fills = getArray(info.fills,figma.variables.getVariableById(styleNodes.fills.fills?.[0]?.boundVariables?.['color'].id)?.name) ;
+        }
+        if (styleNodes.stroke) {
+          info.stroke = getArray(info.stroke,getStyleName(styleNodes.stroke.strokeStyleId));
+        }
+        if (styleNodes.strokes) {
+          info.strokes = getArray(info.strokes,figma.variables.getVariableById(styleNodes.strokes.strokes?.[0]?.boundVariables?.['color'].id)?.name);
+        }
+        if (styleNodes.effect) {
+          info.effect = getArray(info.effect,getStyleName(styleNodes.effect.effectStyleId));
+        }
+        if (styleNodes.text) {
+          info.text = getArray(info.text,getStyleName(styleNodes.text.textStyleId));
+        }
+        if (styleNodes.textStyle) {
+          info.textStyle = getArray(info.textStyle,getStyleName(styleNodes.text.fillStyleId));
+        }
+        if (childNode.height) {
+          info.height = getArray(info.height,childNode.height);
+        }
+        if (styleNodes.radius) {
+          info.radius = getArray(info.radius,[styleNodes.radius.topLeftRadius,styleNodes.radius.topRightRadius,styleNodes.radius.bottomRightRadius,styleNodes.radius.bottomLeftRadius]);
+        }
+        if (styleNodes.padding) {
+          info.padding = getArray(info.padding,[styleNodes.padding.paddingTop,styleNodes.padding.paddingRight,styleNodes.padding.paddingBottom,styleNodes.padding.paddingLeft]);
+        }
+        if (nodes.parent.type === 'INSTANCE') {
+          info.defaultVariant = nodes.children?.variantProperties}
+        if(nodes.parent.type === 'COMPONENT_SET'){
+            info.defaultVariant = Object.keys(nodes.parent.variantGroupProperties);
+        }
+        if(nodes.parent.type === 'COMPONENT'){
+          info.defaultVariant = nodes.children?.variantProperties;
+        }
+    });}
+}
+
+function getNodeInfo() {
+  let nodesInfos: any[] = []; // 배열로 초기화
+  if (nodesInfos.length>0) return;
+    const isPages = getPages();
+    isPages.forEach((page: any) => {
+      //필터된 노드
+      const isNodes = page.children.filter((node: any) => getNodes(node));
+      const nodesInfo:any = {
+        pageName: page.name,
+        pageCount: page.length,
+        nodes:[],
+      };
+      let newNodesWrap: Node[] = [];
+      isNodes.forEach((nodes: any) => {
+        if(nodes.type === "SECTION" && nodes.name.includes("🚫") && nodes.children){
+          newNodesWrap = [...newNodesWrap, ...nodes.children];
+        }else{
+          newNodesWrap = [...newNodesWrap, nodes];
+        }
+      })
+
+      newNodesWrap.forEach((nodes: any) => {
+        const nodeInfo = {
+          name: [],
+          fill: [],
+          fills: [],
+          stroke: [],
+          strokes: [],
+          effect: [],
+          text: [],
+          textStyle: [],
+          height: [],
+          radius: [],
+          padding: [],
+          defaultVariant: [],
+          nodeType: []
+        };
+
+        if((nodes.type !=='COMPONENT_SET')){
+          setNodeInfo(nodes, nodeInfo)
+        }
+        if ('children' in nodes) {
+          nodes.children.forEach((childNode: any) => {
+            setNodeInfo(childNode, nodeInfo)
+          });
+        }
+        nodesInfo.nodes = [...nodesInfo.nodes, nodeInfo];
+
+      });
+      nodesInfos = [...nodesInfos, nodesInfo];
+    });
+  return nodesInfos;
+}
+
+function getSelectedNodeInfo(selectedNodes: any) {
+  let seletedNodes: any[] = []; // 배열로 초기화
+  const seletedNode = {
+    name: [],
+    fill: [],
+    fills: [],
+    stroke: [],
+    strokes: [],
+    effect: [],
+    text: [],
+    textStyle: [],
+    height: [],
+    radius: [],
+    padding: [],
+    defaultVariant: [],
+    nodeType: []
+  };  
+  selectedNodes.forEach((selectedNode)=>{
+    setNodeInfo(selectedNode, seletedNode)
+    return seletedNodes = [...seletedNodes,seletedNode];;
+  })
+  return  seletedNodes;
+}
+
+figma.showUI(__html__, { width: 900, height: 600, title: 'CDS Asset Filter' });
 
 figma.ui.onmessage = (message) => {
   switch(message.type){
-    case 'request_create':
-      const { nodeType } = message;
-      return getStack(nodeType)
+    case 'request_infos':
+      const nodesInfos = getNodeInfo()
+      figma.ui.postMessage({
+        type: 'get_infos',
+        nodesInfos,  
+      });
+      const nodeNames = nodesInfos.map((node) => ({
+        pageName: node.pageName,
+        nodes: node.nodes.map((node: { name: any }) => node.name),
+      }));
+      figma.ui.postMessage({
+        type: 'get_names',
+        nodeNames,
+      });      
+      const propertys = nodesInfos.flatMap(node => 
+        node.nodes.flatMap(node => node.defaultVariant)
+      );
+      const property = [...new Set(propertys)].sort();
+      figma.ui.postMessage({
+        type: 'get_Property',
+        property,
+      });  
+      break;
+    case 'request_selected':
+      const seletedNode = getSelectedNodeInfo(figma.currentPage.selection)  
+      figma.ui.postMessage({
+        type: 'get_selected',
+        seletedNode,
+      });
+      break;
+    case 'request_reset':
+      figma.ui.postMessage({
+        type: 'get_reset',
+        seletedNode: null,
+      });
+      break;
+    case 'open-external-link':
+      figma.notify(`브라우저에서 붙여넣기`);
+      break;
   }
 };
